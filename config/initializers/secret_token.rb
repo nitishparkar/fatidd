a894f089ea593de862c9a03ed4525a6ce5f37ef9
Nitish <developer.nitish@gmail.com>
2013-09-18 15:32:29 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fatidd::Application.config.secret_key_base = '224498b76287d9abbcaae994c6f078d1c744f4b5ab91b1e743cadbc3ed4341a3ab4849c8b6ac54759c482231c63cf7f8aadf09b1d45f1f040a12f79e432fe045'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fatidd::Application.config.session_store :cookie_store, key: '_fatidd_session'
