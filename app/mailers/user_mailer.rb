09827a7f575117c292cea0e13b3a9126eacd8e0b
Nitish <developer.nitish@gmail.com>
2013-10-06 22:03:17 +0530

    added basic mail parsing w/o date

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,15 @@
 class UserMailer < ActionMailer::Base
   def receive(raw)
-    Rails.logger.debug(">>>>> #{raw}")
+    parsed_lines = []
+
+    email = raw.from.first.strip.downcase
+    task_date = Date.today
+    lines = raw.parts.first.body.decoded.split("\n")
+    lines.each do |line|
+      break if line =~ /On .+ <.+@.+> wrote:/
+      parsed_lines << line unless line == ""
+    end
+
+    return parsed_lines, email, task_date
   end
 end
